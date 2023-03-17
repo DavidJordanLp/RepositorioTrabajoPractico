09d7dd7163a4b22d62fa3573bbfcf78e0b26c2cd
DavidJordanLp <djordan1276@gmail.com>
2023-03-17 20:38:33 -0300

    tp

--- TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/Partido.java
+++ TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/Partido.java
@@ -1,15 +1,42 @@
 package trabajoipracticointegrador;
 
 public class Partido extends Equipo{
-  Equipo equipo1;
-  Equipo equipo2 = new Equipo();
+  protected Equipo equipo1; // creo un argumento e instancio el objeto padre Equipo para equipo N° 1
+  protected Equipo equipo2; // creo un argumento e instancio el objeto padre Equipo para equipo N° 2
  
+  protected int golesEquipo1; // creo variable para goles equipo 1
+  protected int golesEquipo2; // creo variable para goles equipo 2
   
-  int golesEquipo1;
-  int golesEquipo2;
-  
+  // constructor vacio
   public Partido() {
+  }
+  
+  // constructor con argumentos
+
+    public Partido(int golesEquipo1, int golesEquipo2, String nombre, String descripcion) {
+        super(nombre, descripcion);
+        this.golesEquipo1 = golesEquipo1;
+        this.golesEquipo2 = golesEquipo2;
     }
+
+    public int getGolesEquipo1() {
+        return golesEquipo1;
+    }
+
+    public void setGolesEquipo1(int golesEquipo1) {
+        this.golesEquipo1 = golesEquipo1;
+    }
+
+    public int getGolesEquipo2() {
+        return golesEquipo2;
+    }
+
+    public void setGolesEquipo2(int golesEquipo2) {
+        this.golesEquipo2 = golesEquipo2;
+    }
+    
+    
+  
 
    
 
--- TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/TrabajoIPracticoIntegrador.java
+++ TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/TrabajoIPracticoIntegrador.java
@@ -1,16 +1,31 @@
 
 package trabajoipracticointegrador;
 
+import java.util.Scanner;
+
 public class TrabajoIPracticoIntegrador {
 
     public static void main(String[] args) {
-       
-        Partido partido1 = new Partido();
-        partido1.equipo1 = new Equipo();
+        Scanner ent = new Scanner(System.in);
+        Partido partido1 = new Partido(); //instancio objeto partido
+        partido1.equipo1 = new Equipo(); //mediante objeto partido
         partido1.equipo2 = new Equipo();
-        partido1.equipo1.nombre = "estudiantes";
-        partido1.equipo2.nombre = "Racing";
-        System.out.println(partido1.equipo2.nombre);
+        
+        System.out.print("Ingrese equipo N° 1: ");
+        partido1.equipo1.setNombre(ent.next());
+        
+        System.out.print("Ingrese equipo N° 2: ");
+        partido1.equipo2.setNombre(ent.next());
+        
+        System.out.print("Igrese cantidad de goles equipo N° 1: ");
+        partido1.setGolesEquipo1(ent.nextInt());
+        
+        System.out.print("Igrese cantidad de goles equipo N° 2: ");
+        partido1.setGolesEquipo1(ent.nextInt());
+        
+        
+        
+        
         
         
         
