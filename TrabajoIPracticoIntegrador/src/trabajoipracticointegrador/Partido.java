e36208854c8429569df8ef268bf4d8579045b002
DavidJordanLp <djordan1276@gmail.com>
2023-03-17 19:57:27 -0300

    primerCommit

---
+++ TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/Partido.java
@@ -0,0 +1,28 @@
+package trabajoipracticointegrador;
+
+public class Partido extends Equipo{
+  Equipo equipo1;
+  Equipo equipo2 = new Equipo();
+ 
+  
+  int golesEquipo1;
+  int golesEquipo2;
+  
+  public Partido() {
+    }
+
+   
+
+    
+  
+  
+
+    
+  
+  
+  
+    
+  
+   
+    
+}
---
+++ TrabajoIPracticoIntegrador/src/trabajoipracticointegrador/TrabajoIPracticoIntegrador.java
@@ -0,0 +1,25 @@
+
+package trabajoipracticointegrador;
+
+public class TrabajoIPracticoIntegrador {
+
+    public static void main(String[] args) {
+       
+        Partido partido1 = new Partido();
+        partido1.equipo1 = new Equipo();
+        partido1.equipo2 = new Equipo();
+        partido1.equipo1.nombre = "estudiantes";
+        partido1.equipo2.nombre = "Racing";
+        System.out.println(partido1.equipo2.nombre);
+        
+        
+        
+       
+        
+        
+       
+        
+       
+    }
+    
+}
